810f3b0bdd5ccaa308353cc2187ee635b92c6f71
chenbiao <149027310@qq.com>
2024-03-28 14:05:07 +0800

    Readonly

--- transaction.go
+++ transaction.go
@@ -12,6 +12,12 @@ func SetDatasource(ds daog.Datasource) {
 	dataSource = ds
 }
 
+func Readonly(ctx *dgctx.DgContext, workFn func(tc *daog.TransContext) error) error {
+	return daog.AutoTrans(func() (*daog.TransContext, error) {
+		return daog.NewTransContext(dataSource, txrequest.RequestReadonly, ctx.TraceId)
+	}, workFn)
+}
+
 func ReadonlyWithResult[T any](ctx *dgctx.DgContext, workFn func(tc *daog.TransContext) (T, error)) (T, error) {
 	return daog.AutoTransWithResult(func() (*daog.TransContext, error) {
 		return daog.NewTransContext(dataSource, txrequest.RequestReadonly, ctx.TraceId)
