95981c289a7177090c4edeb1e27b1ee9b4d9b006
chenbiao <149027310@qq.com>
2024-05-10 18:01:05 +0800

    NotLogSQL

--- go.mod
+++ go.mod
@@ -3,13 +3,13 @@ module github.com/darwinOrg/daog-ext
 go 1.19
 
 require (
-	github.com/darwinOrg/go-common v0.1.10
-	github.com/darwinOrg/go-logger v0.0.5
+	github.com/darwinOrg/go-common v0.1.46
+	github.com/darwinOrg/go-logger v0.0.6
 	github.com/google/uuid v1.6.0
-	github.com/rolandhe/daog v0.0.32
+	github.com/rolandhe/daog v0.0.34
 )
 
 require (
 	github.com/sirupsen/logrus v1.9.3 // indirect
-	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
 )
--- transaction.go
+++ transaction.go
@@ -14,24 +14,51 @@ func SetDatasource(ds daog.Datasource) {
 
 func Readonly(ctx *dgctx.DgContext, workFn func(tc *daog.TransContext) error) error {
 	return daog.AutoTrans(func() (*daog.TransContext, error) {
-		return daog.NewTransContext(dataSource, txrequest.RequestReadonly, ctx.TraceId)
+		return newReadonlyTransContext(ctx)
 	}, workFn)
 }
 
 func ReadonlyWithResult[T any](ctx *dgctx.DgContext, workFn func(tc *daog.TransContext) (T, error)) (T, error) {
 	return daog.AutoTransWithResult(func() (*daog.TransContext, error) {
-		return daog.NewTransContext(dataSource, txrequest.RequestReadonly, ctx.TraceId)
+		return newReadonlyTransContext(ctx)
 	}, workFn)
 }
 
 func Write(ctx *dgctx.DgContext, workFn func(tc *daog.TransContext) error) error {
 	return daog.AutoTrans(func() (*daog.TransContext, error) {
-		return daog.NewTransContext(dataSource, txrequest.RequestWrite, ctx.TraceId)
+		return newWriteTransContext(ctx)
 	}, workFn)
 }
 
 func WriteWithResult[T any](ctx *dgctx.DgContext, workFn func(tc *daog.TransContext) (T, error)) (T, error) {
 	return daog.AutoTransWithResult(func() (*daog.TransContext, error) {
-		return daog.NewTransContext(dataSource, txrequest.RequestWrite, ctx.TraceId)
+		return newWriteTransContext(ctx)
 	}, workFn)
+}
+
+func newReadonlyTransContext(ctx *dgctx.DgContext) (*daog.TransContext, error) {
+	return newTransContextWithRequestStyle(ctx, txrequest.RequestReadonly)
+}
+
+func newWriteTransContext(ctx *dgctx.DgContext) (*daog.TransContext, error) {
+	tc, err := newTransContextWithRequestStyle(ctx, txrequest.RequestWrite)
+	if tc != nil {
+		tc.ExtInfo = map[string]any{
+			"op_id": ctx.OpId,
+		}
+	}
+	return tc, err
+}
+
+func newTransContextWithRequestStyle(ctx *dgctx.DgContext, requestStyle txrequest.RequestStyle) (*daog.TransContext, error) {
+	tc, err := daog.NewTransContext(dataSource, requestStyle, ctx.TraceId)
+	if err != nil {
+		return tc, err
+	}
+
+	if tc.LogSQL && ctx.NotLogSQL {
+		tc.LogSQL = false
+	}
+
+	return tc, err
 }
