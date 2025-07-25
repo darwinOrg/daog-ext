e458cbf0317eb37f792c0129617df123c0663e59
chenbiao <149027310@qq.com>
2025-07-25 16:15:56 +0800

    QueryRawList

--- go.mod
+++ go.mod
@@ -3,10 +3,10 @@ module github.com/darwinOrg/daog-ext
 go 1.23.1
 
 require (
-	github.com/darwinOrg/go-common v0.1.91
+	github.com/darwinOrg/go-common v0.2.0
 	github.com/darwinOrg/go-logger v0.0.12
 	github.com/go-sql-driver/mysql v1.9.3
-	github.com/rolandhe/daog v0.0.40
+	github.com/rolandhe/daog v0.0.41
 )
 
 require (
@@ -16,5 +16,5 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/natefinch/lumberjack v2.0.0+incompatible // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
-	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/sys v0.34.0 // indirect
 )
--- query.go
+++ query.go
@@ -3,6 +3,7 @@ package daogext
 import (
 	dgctx "github.com/darwinOrg/go-common/context"
 	dgerr "github.com/darwinOrg/go-common/enums/error"
+	"github.com/darwinOrg/go-common/utils"
 	dglogger "github.com/darwinOrg/go-logger"
 	"github.com/rolandhe/daog"
 )
@@ -26,4 +27,20 @@ func CountRawByTc(ctx *dgctx.DgContext, tc *daog.TransContext, sql string, args 
 	}
 
 	return *scs[0], nil
+}
+
+func QueryRawList[T any](ctx *dgctx.DgContext, sql string, args ...any) ([]*T, error) {
+	return ReadonlyWithResult(ctx, func(tc *daog.TransContext) ([]*T, error) {
+		return QueryRawListByTc[T](ctx, tc, sql, args...)
+	})
+}
+
+func QueryRawListByTc[T any](ctx *dgctx.DgContext, tc *daog.TransContext, sql string, args ...any) ([]*T, error) {
+	list, err := daog.QueryRawSQL(tc, func(ins *T) []any {
+		return utils.ReflectAllFieldValuePointers(ins)
+	}, sql, args...)
+	if err != nil {
+		dglogger.Errorf(ctx, "daog.QueryRawSQL error: %v", err)
+	}
+	return list, err
 }
